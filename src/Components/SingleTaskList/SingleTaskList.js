41f57b7d937c8dd255cd1b53e32f91421066fb8c
barnes-joseph <barnsjoseph05@gmail.com>
2022-02-14 12:12:28 +0000

    Added dispatch to handle single tasklist state

--- src/Components/SingleTaskList/SingleTaskList.js
+++ src/Components/SingleTaskList/SingleTaskList.js
@@ -1,95 +1,144 @@
-import React,{useContext,useEffect,useState} from 'react';
-import {FcTodoList} from 'react-icons/fc'
-import {Container,Header,Close,TaskTitle,TaskList,ListItem,CheckBox,TaskInput,Add} from './styled'
-import {TasksContext} from '../../Context/TaskProvider'
-import {Link,useParams} from 'react-router-dom'
-import {FiTrash} from 'react-icons/fi'
-import {ACTIONS} from '../../Reducers/TaskActions'
+import React, { useContext, useEffect, useState } from "react";
+import { FcTodoList } from "react-icons/fc";
+import {
+  Container,
+  Header,
+  Close,
+  TaskTitle,
+  TaskList,
+  ListItem,
+  CheckBox,
+  TaskInput,
+  Add,
+} from "./styled";
+import { TasksContext } from "../../Context/TaskProvider";
+import { Link, useParams } from "react-router-dom";
+import { FiTrash } from "react-icons/fi";
+import { ACTIONS } from "../../Reducers/TaskActions";
+import { fetchTaskListById } from "../../Api/tasklist-api";
+import { addTask, deleteTask, toggleTask } from "../../Api/tasks-api";
 
 const SingleTaskList = () => {
-  const {id} = useParams()
-  const {tasks,dispatch} = useContext(TasksContext)
-  const [taskList,setTaskList] = useState({id:null,name:'',tasks:[],completed:0})
-  const [newTask,setNewTask] = useState('')
+  const { id } = useParams();
+  const { task, dispatchTasks } = useContext(TasksContext);
+  const [newTask, setNewTask] = useState("");
 
   const handleChange = (e) => {
-    setNewTask(e.target.value)
-  }
+    setNewTask(e.target.value);
+  };
 
   const handleKeyPress = (e) => {
-    console.log('key pressed')
-    if(e.key === "Enter"){
-      console.log('Enter key detected')
-      onAddTask()
+    console.log("key pressed");
+    if (e.key === "Enter") {
+      console.log("Enter key detected");
+      onAddTask();
     }
-  }
+  };
 
-  const onAddTask = () => {
-    if(newTask !== ''){
-    dispatch({type:ACTIONS.ADD_TASK,payload:{task_list_id:parseInt(id),task:newTask}})
-    setNewTask('')
+  const onAddTask = async () => {
+    if (newTask !== "") {
+      const task = await addTask(id,{name:newTask})
+      if(task.success){
+      dispatchTasks({
+        type: ACTIONS.ADD_TASK,
+        payload: { task: task.task },
+      })}
+      setNewTask("");
     }
-  }
+  };
 
-  const onDeleteTask = (task_id) => {
-    dispatch({type:ACTIONS.DELETE_TASK,payload:{task_list_id:parseInt(id),task_id:task_id}})
-    dispatch({
-      type: ACTIONS.COMPLETED,
-      payload: { task_list_id: parseInt(id) },
-    });
-  }
-
-  const onCheck = (task) => {
-    const new_task = {...task,complete:!task.complete}
-    dispatch({type:ACTIONS.TOGGLE,payload:{task_list_id:parseInt(id),task:new_task}})
-    dispatch({type:ACTIONS.COMPLETED,payload:{task_list_id:parseInt(id)}})
-  }
-
-  const fetchTaskList = () => {
-    for (const task of tasks){
-      if(task.id === parseInt(id)){
-        setTaskList(task)
-      }
+  const onDeleteTask = async (task_id) => {
+    const deleted_task = await deleteTask(task_id)
+    if(deleted_task.success){
+      dispatchTasks({type:ACTIONS.DELETE_TASK,payload:{task_id:parseInt(task_id)}})
     }
-  }
+    else{
+      alert(deleted_task.message)
+    }
+  };
 
-  useEffect(()=>{
-      fetchTaskList()
-  },[])
+  const onCheck = async (task) => {
+    const update_data = {complete: !task.complete };
+    const update_task = await toggleTask(id,task.id,update_data)
+    if(update_task.success){
+      console.log(update_task.task)
+      dispatchTasks({
+        type: ACTIONS.TOGGLE,
+        payload: { task: update_task.task,completed:update_task.completed_tasks },
+      });
+    }
+    else{
+      alert(update_task.message)
+    }
+  };
+
+  const fetchTasks = async () => {
+    const tasks = await fetchTaskListById(id);
+    if (tasks.success) {
+      dispatchTasks({
+        type: ACTIONS.FETCH_TASK_LIST,
+        payload: { tasks: tasks.tasklist },
+      }); 
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
   return (
-    <div style={{'width':'100vw','display':'flex','flexDirection':'column'}}>
-    <Container>
+    <div style={{ width: "100vw", display: "flex", flexDirection: "column" }}>
+      <Container>
         <Header>
-          <FcTodoList size={24}/>
-          <Link to='/' style={{'textDecoration':'none','color':'black'}}>
+          <FcTodoList size={24} />
+          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
             <Close>x</Close>
           </Link>
         </Header>
         <TaskTitle>
-            <h2 style={{'marginBottom':'0.2rem'}}>{taskList.name}</h2>
-            <span style={{'color':'grey'}}>{taskList.completed} of {taskList.tasks.length} tasks</span>
+          <h2 style={{ marginBottom: "0.2rem" }}>{task.name}</h2>
+          <span style={{ color: "grey" }}>
+            {task.completed} of {task.tasks.length} tasks
+          </span>
         </TaskTitle>
-        <hr style={{'marginLeft':'3rem','width':'80%'}}/>
-        <div style={{'alignSelf':'center','width':'60%','marginTop':'1.5rem','display':'flex'}}>
-          <TaskInput type='text' value={newTask} onChange={handleChange} onKeyDown={handleKeyPress}/>
+        <hr style={{ marginLeft: "3rem", width: "80%" }} />
+        <div
+          style={{
+            alignSelf: "center",
+            width: "60%",
+            marginTop: "1.5rem",
+            display: "flex",
+          }}
+        >
+          <TaskInput
+            type="text"
+            value={newTask}
+            onChange={handleChange}
+            onKeyDown={handleKeyPress}
+          />
           <Add onClick={onAddTask}> + </Add>
         </div>
-        
+
         <TaskList>
-        {taskList.tasks.length !== 0 && taskList.tasks.map(task=>{
-          console.log(task)
-          return (
-            <ListItem key={task.id}>
-                    <CheckBox type='checkbox' checked={task.complete === true ? "checked" : null} onChange={()=>onCheck(task)}/>
-                    <span>{task.name}</span>
-                    <FiTrash style={{'marginLeft':'auto','alignSelf':'center'}} onClick={()=>onDeleteTask(task.id)}/>
-            </ListItem>
-          )}
-        )}
+          {task.tasks.length !== 0 &&
+            task.tasks.map((task) => {
+              return (
+                <ListItem key={task.id}>
+                  <CheckBox
+                    type="checkbox"
+                    checked={task.complete === true ? "checked" : null}
+                    onChange={() => onCheck(task)}
+                  />
+                  <span>{task.name}</span>
+                  <FiTrash
+                    style={{ marginLeft: "auto", alignSelf: "center" }}
+                    onClick={() => onDeleteTask(task.id)}
+                  />
+                </ListItem>
+              );
+            })}
         </TaskList>
-        
-    </Container>
+      </Container>
     </div>
   );
 };
