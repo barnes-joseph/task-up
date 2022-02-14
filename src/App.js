f1f23866971dbb3ee0a092daaa408e99460b9cbf
barnes-joseph <barnsjoseph05@gmail.com>
2022-02-14 12:10:35 +0000

    Added a provider for tasklist

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from 'react'
 import SingleTaskList from './Components/SingleTaskList/SingleTaskList'
 import TaskLists from './Components/TaskLists/TaskLists'
 import TaskProvider from './Context/TaskProvider'
+import TaskListProvider from './Context/TaskListProvider'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <TaskProvider>
-      <Router>
-        <Routes>
-          <Route element={<TaskLists/>} path='/'/>
-          <Route element={<SingleTaskList/>} path='/tasks/:id'/>
-        </Routes>
-      </Router>
-    </TaskProvider>
+    <TaskListProvider>
+      <TaskProvider>
+        <Router>
+          <Routes>
+            <Route element={<TaskLists />} path="/" />
+            <Route element={<SingleTaskList />} path="/tasks/:id" />
+          </Routes>
+        </Router>
+      </TaskProvider>
+    </TaskListProvider>
   );
 }
 
