4412186ee9e671ce1bd0886ab1255798bb8d921b
barnes-joseph <barnsjoseph05@gmail.com>
2022-02-14 12:13:57 +0000

    modified task provider

--- src/Context/TaskProvider.js
+++ src/Context/TaskProvider.js
@@ -1,12 +1,12 @@
 import React,{useReducer} from 'react';
 import {taskReducer} from '../Reducers/TaskReducer'
 
-export const TasksContext = React.createContext()
+export const TasksContext = React.createContext({name:'',completed:0,tasks:[]})
 
 const TaskProvider = ({children}) => {
-    const [tasks,dispatch] = useReducer(taskReducer,[])
+    const [task,dispatchTasks] = useReducer(taskReducer,{name:'',completed:0,tasks:[]})
     return (
-        <TasksContext.Provider value={{tasks,dispatch}}>
+        <TasksContext.Provider value={{task,dispatchTasks}}>
             {children}
         </TasksContext.Provider>
     )
