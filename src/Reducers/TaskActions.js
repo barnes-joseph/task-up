75ce1089e493aa33234f18e3cbbe6ceaf9c998dc
barnes-joseph <barnsjoseph05@gmail.com>
2022-02-14 12:14:10 +0000

    added new actions to the task actions

--- src/Reducers/TaskActions.js
+++ src/Reducers/TaskActions.js
@@ -1,9 +1,10 @@
 export const ACTIONS = {
+    FETCH_TASK_LISTS:'fetch_task_lists',
     ADD_TASK_LIST : 'add_task_list',
     FETCH_TASK_LIST : 'fetch_task_list',
     ADD_TASK : 'add_task',
     DELETE_TASK_LIST : 'delete_task_list',
     DELETE_TASK : 'delete_task',
     TOGGLE : 'toggle',
-    COMPLETED: 'completed'
+    COMPLETED:' completed'
 }
